710d43ca27ad651998bbf17d8fad38403549a274
jspark <namic123@naver.com>
2025-03-14 16:32:15 +0900

    QueryDsl Case문 기본 예제

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,8 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -486,4 +488,66 @@ public class QuerydslBasicTest {
             3. nativeSQL을 사용한다.
     * */
 
+
+    /*
+    * Case 문
+    * */
+    @Test
+    public void basicCase(){
+        List<String> result = jpaQueryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타")
+                ).from(member)
+                .fetch();
+
+        for(String s : result){
+            System.out.println("s = " + s);
+        }
+    }
+
+    /*
+    * 복잡한 케이스
+    * */
+    @Test
+    public void complexCase(){
+        List<String> result = jpaQueryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타")
+                ).from(member)
+                .fetch();
+        
+        for(String s : result){
+            System.out.println("s = " + s);
+        }
+    }
+
+
+    /*
+    * 임의의 순서로 회원을 출력하고 싶은 경우
+    *   1. 0 ~ 30살이 아닌 회원을 가장 먼저 출력
+        2. 0 ~ 20살 회원 출력
+        3. 21 ~ 30살 회원 출력
+    * */
+    public void optionalOrderCase (){
+        NumberExpression<Integer> rankPath = new CaseBuilder()
+                .when(member.age.between(0, 20)).then(2)
+                .when(member.age.between(21, 30)).then(1)
+                .otherwise(3);
+        List<Tuple> result = jpaQueryFactory
+                .select(member.username, member.age, rankPath)
+                .from(member)
+                .orderBy(rankPath.desc())
+                .fetch();
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            Integer rank = tuple.get(rankPath);
+            System.out.println("username = " + username + " age = " + age + " rank = " +
+                    rank);
+        }
+    }
 }
