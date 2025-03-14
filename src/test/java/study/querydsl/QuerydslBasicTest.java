5acf7e0edfbf890cd02954dd8a0a0654f9dfcfe5
jspark <namic123@naver.com>
2025-03-14 16:22:55 +0900

    QueryDsl 서브쿼리 기본 예제

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -393,5 +394,96 @@ public class QuerydslBasicTest {
                 emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
         assertThat(loaded).as("페치 조인 적용").isTrue();
     }
+
+    
+    /*
+    * 서브 쿼리
+    * 나이가 가장 많은 회원 조회
+    * */
+    @Test
+    public void subQuery(){
+        QMember memberSub = new QMember("memberSub");
+        
+        Member findOldestMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        JPAExpressions
+                                .select(member.age.max())
+                                .from(member)
+                )).fetchOne();
+
+        System.out.println("findOldestMember = " + findOldestMember);
+    }
+
+    /*
+    * 나이가 평균 나이 이상인 회원
+    * */
+    @Test
+    public void subQueryGoe(){
+        List<Member> findMoreOldestMemberThanAvg = jpaQueryFactory
+                .selectFrom(member)
+                /* member.age >= member.age.avg*/
+                .where(member.age.goe(
+                        JPAExpressions
+                                .select(member.age.avg())
+                                .from(member)
+                )).fetch();
+
+                for(Member member : findMoreOldestMemberThanAvg){
+                    System.out.println("member = " + member);
+                }
+    }
+    /**
+     * 서브쿼리 여러 건 처리, in 사용
+     */
+    @Test
+    public void subQueryIn() throws Exception {
+        QMember memberSub = new QMember("memberSub");
+        List<Member> result = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                        JPAExpressions
+                                .select(memberSub.age)
+                                .from(memberSub)
+                                .where(memberSub.age.gt(10))
+                ))
+                .fetch();
+
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    /*
+    * Select 절에 서브 쿼리
+    * */
+    @Test
+    public void subQueryInSelect(){
+        List<Tuple> fetch = jpaQueryFactory
+                .select(member.username,
+                        JPAExpressions
+                                .select(member.age.avg())
+                                .from(member)
+                ).from(member)
+                .fetch();
+        for (Tuple tuple : fetch) {
+            System.out.println("username = " + tuple.get(member.username));
+            System.out.println("age = " +
+                    tuple.get(JPAExpressions.select(member.age.avg())
+                            .from(member)));
+        }
+    }
+
+    /*
+    * from 절의 서브쿼리 한계
+        JPA JPQL 서브쿼리의 한계점으로 from 절의 서브쿼리(인라인 뷰)는 지원하지 않는다. 당연히 Querydsl도 지원하지
+        않는다. 하이버네이트 구현체를 사용하면 select 절의 서브쿼리는 지원한다. Querydsl도 하이버네이트 구현체를 사용
+        하면 select 절의 서브쿼리를 지원한다.
+        *
+        from 절의 서브쿼리 해결방안
+            1. 서브쿼리를 join으로 변경한다. (가능한 상황도 있고, 불가능한 상황도 있다.)
+            2. 애플리케이션에서 쿼리를 2번 분리해서 실행한다.
+            3. nativeSQL을 사용한다.
+    * */
 
 }
